f2a01d680820bd4e4141925ea6b4b3d13578e020
Ciulica Alex <alexandruciulica2@gmail.com>
2025-12-15 19:33:47 +0200

    generare matrice 1

--- main.cpp
+++ main.cpp
@@ -1,34 +1,19 @@
     #include<iostream>
     using namespace std;
-    int a[101][101],n,m,k=0;
+    int a[25][25],n;
     int main() {
-        cin>>n>>m;
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=m;j++) {
-                cin>>a[i][j];
+        cin>>n;
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=n;j++) {
+                a[i][j]=(i*j)%10;
             }
         }
-        for(int i=2;i<=n;i=i+2) {
-            for(int j=1;j<=m;j++) {
-                int nrp=0;
-                 if (i%2==0) {
-                     for (int d=1;d*d<=a[i][j];d++) {
-                         if (a[i][j]%d==0) {
-                             nrp++;
-                             if (a[i][j]/d!=d) {
-                                 nrp++;
-                             }
-                         }
-                     }
-                 }
-                if (nrp==2){
-                    k++;
+        for (int i=1;i<=n;i++) {
+            for (int j=1;j<=n;j++) {
+                cout<<a[i][j]<<" ";
             }
-
-            }
-
+            cout<<endl;
         }
-        cout<<k;
 
 
         return 0;
