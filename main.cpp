827791647c426e56110474dbdd5dfa46dae2638f
Ciulica Alex <alexandruciulica2@gmail.com>
2025-12-22 13:51:28 +0200

    nr de cifre matrice

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
-    #include<iostream>
-    using namespace std;
-    int a[25][25],n;
-    int main() {
-        cin>>n;
-        //abc
-        for (int i=1;i<=n;i++) {
-            for (int j=1;j<=n;j++) {
-                a[i][j]=(i*j)%10;
-            }
-        }
-        for (int i=1;i<=n;i++) {
-            for (int j=1;j<=n;j++) {
-                cout<<a[i][j]<<" ";
-            }
-            cout<<endl;
-        }
-
-
-        return 0;
+#include <iostream>
+using namespace std;
+int n,nr=0,a[10];
+int main(){
+  cin>>n;
+    int y=n;
+    while (y>0) {
+        nr++;
+        a[nr]=y%10;
+        y/=10;
     }
+    for (int i=1;i<=nr;i++) {
+        for (int j=nr;j>=1;j--) {
+            cout<<a[j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
