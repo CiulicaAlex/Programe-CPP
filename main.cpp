aadbfee1743501dcf4a8b5e3f36641b4a4ff8dce
Ciulica Alex <alexandruciulica2@gmail.com>
2025-12-23 10:05:11 +0200

    eliminare diagonala principala

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int n;
+int n,a[101][101];
 
 int main() {
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (i + j <= n + 1)
-                cout << i + j - 1 << " ";
-            else
-                cout << 2 * n - (i + j) + 1 << " ";
+   cin>>n;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            cin>>a[i][j];
         }
-        cout << endl;
     }
-
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            if (i!=j){
+                cout<<a[i][j]<<" ";
+        }
+    }
+    cout<<endl;
+}
     return 0;
 }
