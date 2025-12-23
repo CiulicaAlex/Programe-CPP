4df70a76d75a6e0c05e8d2d8cde63381e22cb18b
Ciulica Alex <alexandruciulica2@gmail.com>
2025-12-23 10:25:55 +0200

    sub bac 2025 13

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-
-int n,a[101][101];
+ifstream f("input.txt");
+int n,m,a[101][101];
 
 int main() {
-   cin>>n;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=n;j++) {
-            cin>>a[i][j];
+   f>>m>>n;
+    for (int i=1;i<=m;i++) {
+        for (int j=1;j<=n;j++) {
+            f>>a[i][j];
         }
     }
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=n;j++) {
-            if (i+j!=n+1){
-                cout<<a[i][j]<<" ";
+    int minim=INT_MAX;
+    int indLin=-1, indCol=-1;
+    for (int i=1;i<=m;i++) {
+        for (int j=1;j<=n;j++) {
+            if (a[i][j]<minim) {
+                minim=a[i][j];
+                indLin=i;
+                indCol=j;
+            }
         }
     }
-    cout<<endl;
-}
+    int ultimul=a[m][n];
+    for (int i=1;i<=m;i++) {
+        a[i][indCol]=ultimul;
+    }
+    for (int i=1;i<=m;i++) {
+        for (int j=1;j<=n;j++) {
+            cout<<a[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
     return 0;
 }
