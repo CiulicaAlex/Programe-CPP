8ff31f4059e2cf457624131f2e78e374308d10af
Ciulica Alex <alexandruciulica2@gmail.com>
2025-12-24 11:10:08 +0200

    prod div impari interval

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
-int n,m,a[101][101];
+int a,b,maxx=0;
 int main() {
-   cin>>m>>n;
-    for (int i=0;i<m;i++) {
-        for (int j=0;j<n;j++) {
-            cin>>a[i][j];
+cin>>a>>b;
+    for (int i=a;i<=b;i++) {
+        int P=1;
+        for (int d=1;d<=i;d++) {
+            if (i%d==0 && d%2==1) {
+                P=P*d;
+            }
         }
-    }
-    int maxx=0;
-    for (int i=0;i<m-1;i++) {
-        for (int j=0;j<n-1;j++) {
-            int S=a[i][j]+a[i+1][j]+a[i+1][j+1]+a[i][j+1];
-            if (S>maxx)
-                maxx=S;
+        if (P>i) {
+            maxx=i;
+        }
 
-        }
     }
-    cout<<maxx;
+cout<<maxx;
     return 0;
 }
