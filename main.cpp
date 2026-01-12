146feaa94c5f05967dee8947b071325e37d9d984
Ciulica Alex <alexandruciulica2@gmail.com>
2026-01-12 18:32:28 +0200

    Prime4 Pbinfo

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
+#include <iostream>
 #include <fstream>
 using namespace std;
-
-int fr[10000]; // fr[i] = 1 dacă i apare în listă
-
-int main() {
-    ifstream fin("numere8.in");
-    ofstream fout("numere8.out");
-
-   int x;
-    while (fin>>x) {
-        if (x<=9999 && x>0) {
-            fr[x]=1;
+int fr[1000];
+int n;
+int main(){
+    ifstream fin("prime4.in");
+ofstream fout("prime4.out");
+    fin>>n;
+    for (int i=1;i<=n;i++) {
+        int x;
+        fin>>x;
+        int nrd=0;
+       for (int d=2;d*d<=x;d++) {
+           if (x%d==0) {
+               nrd++;
+               if (d!=x/d) {
+                   nrd++;
+               }
+           }
+       }
+        if (nrd==0) {
+            fr[x]++;
         }
     }
-for (int i=9999;i>=1;i--) {
-    if (fr[i]==0) {
-        fout<<i<<" ";
-    }
+    int frmax=0;
+    int rezultat=0;
+    for (int i=100;i<=999;i++) {
+if (fr[i]>frmax) {
+    frmax=fr[i];
+    rezultat=i;
 }
+    }
+   fout<<rezultat;
+
+
     return 0;
 }
