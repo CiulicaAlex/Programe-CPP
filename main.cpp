1b721f570094475bad28a04f98835943cc0ac8f6
Ciulica Alex <alexandruciulica2@gmail.com>
2026-01-14 19:55:37 +0200

    probleme matrice scoala

--- main.cpp
+++ main.cpp
@@ -1,38 +1,124 @@
-#include <iostream>
-#include <fstream>
+/*
+ *#include <iostream>
 using namespace std;
-int fr[1000];
-int n;
+int m,n,a[101][101];
 int main(){
-    ifstream fin("prime4.in");
-ofstream fout("prime4.out");
-    fin>>n;
-    for (int i=1;i<=n;i++) {
-        int x;
-        fin>>x;
-        int nrd=0;
-       for (int d=2;d*d<=x;d++) {
-           if (x%d==0) {
-               nrd++;
-               if (d!=x/d) {
-                   nrd++;
-               }
-           }
-       }
-        if (nrd==0) {
-            fr[x]++;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            cin>>a[i][j];
         }
     }
-    int frmax=0;
-    int rezultat=0;
-    for (int i=100;i<=999;i++) {
-if (fr[i]>frmax) {
-    frmax=fr[i];
-    rezultat=i;
-}
+    for(int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            int uc=a[i][j]%10;
+            cout<<uc<<" ";
+        }
+        cout<<endl;
     }
-   fout<<rezultat;
-
 
     return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int a[101][101];
+int main() {
+    int m,n;
+    cin>>m>>n;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) {
+            cin>>a[i][j];
+        }
+    }
+    int p=1;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=m;j++) {
+            if (a[i][j]%2==0) {
+                p=p*a[i][j];
+            }
+        }
+    }
+    cout<<p;
+
+return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int a[101][101];
+int main() {
+    int m,n,nr=0;
+    cin>>n>>m;
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            cin>>a[i][j];
+        }
+    }
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            if (a[i][j]<0) {
+                nr++;
+            }
+        }
+    }
+cout<<nr;
+
+return 0;
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int a[101][101];
+int main() {
+    int n,m;
+    cin>>n>>m;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            cin>>a[i][j];
+        }
+    }
+    int s=0;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            if (a[i][j]%5==0) {
+                s+=a[i][j];
+            }
+        }
+    }
+    cout<<s;
+return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+int a[101][101];
+int main() {
+int n,m;
+    cin>>n>>m;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            cin>>a[i][j];
+        }
+    }
+    int nrp=0,nrimp=0;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            if (a[i][j]%2==0) {
+                nrp++;
+            }
+            else {
+                nrimp++;
+            }
+        }
+    }
+    cout<<nrp<<" "<<nrimp<<endl;
+return 0;
 }
