fe3e33e8353fc5a22866a208e444103d73e97e50
Ciulica Alex <alexandruciulica2@gmail.com>
2026-01-27 20:07:13 +0200

    sortare crescatoare sume pe zone matrice

--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
 using namespace std;
-int a[101][101],n,m;
-int main(){
-cin>>n>>m;
-    for(int i=1;i<=n;i++) {
-        for(int j=1;j<=m;j++) {
+int n,a[101][101];
+int main() {
+    cin>>n;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {
             cin>>a[i][j];
         }
     }
-    int ue=a[n][m];
-    for (int j=1;j<=m;j++) {
-        a[1][j]=ue;
-    }
-    for (int j=1;j<=m;j++) {
-        a[n][j]=ue;
-    }
-    for (int i=2;i<=n-1;i++) {
-        a[i][1]=ue;
-    }
-    for (int i=2;i<=n-1;i++) {
-        a[i][m]=ue;
-    }
+    int S1=0,S2=0,S3=0,S4=0;
     for (int i=1;i<=n;i++) {
-        for (int j=1;j<=m;j++) {
-            cout<<a[i][j]<<" ";
+        for (int j=1;j<=n;j++) {
+            if (j>i && i+j<n+1) {
+                S1=S1+a[i][j];
+            }
+            else if (j>i && i+j>n+1) {
+                S2=S2+a[i][j];
+            }
+            else if (i>j && i+j>n+1) {
+                S3=S3+a[i][j];
+            }
+            else if (i>j && i+j<n+1) {
+                S4=S4+a[i][j];
+            }
         }
-        cout<<endl;
     }
+    if (S1>S2) swap(S1,S2);
+    if (S1>S3) swap(S1,S3);
+    if (S1>S4) swap(S1,S4);
+    if (S2>S3) swap(S2,S3);
+    if (S2>S4) swap(S2,S4);
+    if (S3>S4) swap(S3,S4);
+    cout<<S1<<" "<<S2<<" "<<S3<<" "<<S4;
 
 
-return 0;
+
+    return 0;
 }
