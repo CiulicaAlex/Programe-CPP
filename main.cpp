a26a91b6e154c9ae38285cf8328a11567314e6c1
Ciulica Alex <alexandruciulica2@gmail.com>
2026-01-31 13:02:15 +0200

    Refactor main.cpp to support dynamic matrix dimensions and simplify output logic

--- main.cpp
+++ main.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
+using namespace std;
 int a[101][101];
+int n,m,s;
 int main() {
-    int n;
-    std::cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            std::cin >> a[i][j];
+    cin>>n>>m;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            cin>>a[i][j];
         }
     }
-    int Sum1 = 0, Sum2 = 0, Sum3 = 0, Sum4 = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (j > i && i + j < n + 1) Sum1 += a[i][j];
-            else if (j > i && i + j > n + 1) Sum2 += a[i][j];
-            else if (i > j && i + j > n + 1) Sum3 += a[i][j];
-            else if (i > j && i + j < n + 1) Sum4 += a[i][j];
+for (s=2;s<=n+m;s++) {
+    for (int i=1;i<=n;i++) {
+        int j=s-i;
+        if (j>=1 && j<=m) {
+            cout<<a[i][j]<<" ";
         }
+
     }
-    std::cout<<"Zona 1(A)=" << Sum1 << " " <<"Zona 2(B)="<< Sum2 << " " <<"Zona 3(C)="<< Sum3 << " " <<"Zona 4(D)="<< Sum4 << std::endl;
-    char fill;
-    if (Sum1 >= Sum2 && Sum1 >= Sum3 && Sum1 >= Sum4) fill = 'A';
-    else if (Sum2 >= Sum1 && Sum2 >= Sum3 && Sum2 >= Sum4) fill = 'B';
-    else if (Sum3 >= Sum1 && Sum3 >= Sum2 && Sum3 >= Sum4) fill = 'C';
-    else fill = 'D';
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            std::cout << fill << " ";
-        }
-        std::cout << std::endl;
-    }
+    cout<<endl;
+}
 
     return 0;
 }
