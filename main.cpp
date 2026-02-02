054e734f45436923d3278c6d371f788ffed75785
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-02 14:26:12 +0200

    GenMat 5 pbinfo

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 using namespace std;
-int n,a[101][101];
+int n;
+int cifre[9],cnt=0;
+int a[101][101];
 int main() {
-    cin>>n;
-    for (int i=1;i<=n;i++) {
-        for (int j=1;j<=n;j++) {
-            if (i==1 || i==n) {
-                a[i][j]=i+j;
-            } else if (j==1 || j==n) {
-                a[i][j]=i+j;
-            } else {
-                a[i][j]=a[i-1][j-1]+a[i-1][j]+a[i-1][j+1];
-            }
+cin>>n;
+    while (n>0) {
+        cifre[cnt]=n%10;
+        cnt++;
+        n=n/10;
+    }
+    for (int i=0;i<cnt;i++) {
+        for (int j=0;j<cnt;j++) {
+            a[i][j]=cifre[j];
+        }
+    }
+    for (int i=0;i<cnt;i++) {
+        for (int j=0;j<cnt;j++) {
             cout<<a[i][j]<<" ";
         }
         cout<<endl;
