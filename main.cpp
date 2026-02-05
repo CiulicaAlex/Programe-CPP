002248af2be71010a73964bbbc349cc022768ce9
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-05 16:09:13 +0200

    234# pbinfo

--- main.cpp
+++ main.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include<fstream>
+#include<iomanip>
 using namespace std;
-ifstream fin("cautanrinmatrice.in");
-ofstream fout("cautanrinmatrice.out");
-int a[1001][1001],n,m,p,caut[1000001];
+ifstream fin("medpoz.in");
+ofstream fout("medpoz.out");
+int a[21][21],n,OK=0;
+double nr=0;
 int main() {
-fin>>n>>m;
-        for (int i=1;i<=n;i++) {
-                for (int j=1;j<=m;j++) {
-                        fin>>a[i][j];
-                }
+fin>>n;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {
+            fin>>a[i][j];
         }
-        fin>>p;
-        for (int i=0;i<p;i++) {
-                fin>>caut[i];
+    }
+    double S=0;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {
+            if (i>j && a[i][j]>0) {
+                S+=a[i][j];
+                nr++;
+                OK=1;
+            }
         }
-        for (int i=0;i<p;i++) {
-                int gasit=0;
-                for (int lin=1;lin<=n && gasit==0;lin++) {
-                        for (int col=1;col<=m && gasit==0;col++) {
-                                if (a[lin][col]==caut[i]) {
-                                        gasit=1;
-                                        fout<<lin<<" "<<col<<"\n";
-                                }
-                        }
-                }
-                if (gasit==0) {
-                        fout<<"0\n";
-                }
-        }
+    }
+    double Ma=S/nr;
+    if (OK!=0) {
+        fout<<fixed<<setprecision(3)<<Ma;
+    } else {
+        fout<<"NU EXISTA";
+    }
 
-
-        return 0;
+    return 0;
 }
