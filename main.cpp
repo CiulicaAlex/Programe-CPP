7aea955a15e440699722a216d94ce76384c76778
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-10 21:38:22 +0200

    4243 pbinfo

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 #include <fstream>
 using namespace std;
-bool crescator(long long x) {
-    int last=10;
-    while (x>0) {
-        int c=x%10;
-        if (c>=last) return false;
-            last=c;
-        x/=10;
-    }
-   return true;
-}
-ifstream fin("saci.in");
-ofstream fout("saci.out");
-int n;
+ifstream fin("mouse.in");
+ofstream fout("mouse.out");
+int n,m,a[101][101],s=0,minim=101;
 int main() {
-fin>>n;
-    int cnt=0;
-    for (int i=0;i<n;i++) {
-        long long x;
-        fin>>x;
-        if (crescator(x)) {
-            cnt++;
+    fin>>n>>m;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            fin>>a[i][j];
+            s+=a[i][j];
+            if ((i+j)%2==1 && minim > a[i][j]) {
+                minim=a[i][j];
+            }
         }
     }
-fout<<cnt;
+    if (n%2==1 || m%2==1) {
+        fout<<n*m<<" "<<s;
+    } else {
+        fout<<n*m-1<<" "<<s-minim;
+    }
+
     return 0;
 }
