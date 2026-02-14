5b85a42dc38dcc51e094022d0f9be4faf1b0229f
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-14 23:13:28 +0200

    #3313 pbinfo

--- main.cpp
+++ main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include<fstream>
 using namespace std;
-ifstream fin("eratostene1.in");
-ofstream fout("eratostene1.out");
+ifstream fin("eratostene2.in");
+ofstream fout("eratostene2.out");
 int main() {
-    int cnt=0;
-    const int MAX=1000000;
-     bool prime[MAX];
-        for (int i=0;i<MAX;i++) {
-            prime[i]=true;
-        }
-        prime[0]=prime[1]=false;
-    for (int i=2;i*i<MAX;i++) {
-            if (prime[i]) {
-                for (int j=i*i;j<MAX;j+=i) {
-                    prime[j]=false;
-                }
+    bool prime[1000001];
+    for (int i=0;i<=1000001;i++) {
+        prime[i]=true;
+    }
+    prime[0]=prime[1]=false;
+    for (int i=2;i*i<=1000000;i++) {
+        if (prime[i]) {
+            for (int j=i*i;j<=1000000;j+=i) {
+                prime[j]=false;
             }
         }
+    }
     int n;
     fin>>n;
-    for (int k=1;k<=n;k++) {
+    for (int i=1;i<=n;i++) {
+        int cnt=0;
         int x;
         fin>>x;
-        if (prime[x]) {
-            cnt++;
+        for (int d=2;d*d<=x;d++) {
+            if (prime[d] &K& x%d==0) {
+                cnt++;
+                while (x%d==0) x/=d;
+            }
         }
+        if (x>1) cnt++;
+        fout<<cnt<<" ";
     }
-   fout<<cnt;
 
     return 0;
 }
