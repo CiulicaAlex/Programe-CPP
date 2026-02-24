9bdfc991bbfd9d0fa4137c6cd20d3cb39a55da6a
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-24 14:18:15 +0200

    3903 pbinfo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int fr[200001]={0};
-int main() {
-    int x;
-    while (cin>>x) {
-        fr[x]++;
-    }
-    int cnt=0;
-    for (int i=0;i<200000;i++) {
-        if (fr[i]==1) {
-            cnt++;
-        }
-    }
-    cout<<cnt;
+int a[502][502], s[502][502];
+int N,Q,l1,c1,l2,c2,v;
 
-
+int main()
+{
+    cin>>N;
+    for(int i=1;i<=N;++i)
+        for(int j=1;j<=N;++j)
+            cin>>a[i][j];
+    cin>>Q;
+    while(Q--)
+    {
+        cin>>l1>>c1>>l2>>c2>>v;
+        s[l1][c1]+=v;
+        s[l1][c2+1]-=v;
+        s[l2+1][c1]-=v;
+        s[l2+1][c2+1]+=v;
+    }
+    for(int i=1;i<=N;++i,cout<<'\n')
+        for(int j=1;j<=N;++j)
+            s[i][j]+=s[i-1][j]+s[i][j-1]-s[i-1][j-1],cout<<a[i][j]+s[i][j]<<' ';
     return 0;
 }
