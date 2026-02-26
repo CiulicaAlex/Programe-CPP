8e12751a9652bb42ca48abedff4974f16a5d2076
Ciulica Alex <alexandruciulica2@gmail.com>
2026-02-26 20:40:13 +0200

    4941 pbinfo

--- main.cpp
+++ main.cpp
@@ -1,25 +1,62 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
-int a[502][502], s[502][502];
-int N,Q,l1,c1,l2,c2,v;
-
-int main()
-{
-    cin>>N;
-    for(int i=1;i<=N;++i)
-        for(int j=1;j<=N;++j)
-            cin>>a[i][j];
-    cin>>Q;
-    while(Q--)
-    {
-        cin>>l1>>c1>>l2>>c2>>v;
-        s[l1][c1]+=v;
-        s[l1][c2+1]-=v;
-        s[l2+1][c1]-=v;
-        s[l2+1][c2+1]+=v;
+ifstream fin("dans.in");
+ofstream fout("dans.out");
+int c;
+long long n,D;
+long long a[1000001];
+bool is_prime[2000001];
+bool digits_D[10];
+void build_sieve(long long n) {
+    for (long long i=0;i<=n;i++) is_prime[i]=true;
+    is_prime[0]=is_prime[1]=false;
+    for (long long i=2;i*i<=n;i++) {
+        if (is_prime[i]) {
+            for (long long j=i*i;j<=n;j+=i) {
+                is_prime[j]=false;
+            }
+        }
     }
-    for(int i=1;i<=N;++i,cout<<'\n')
-        for(int j=1;j<=N;++j)
-            s[i][j]+=s[i-1][j]+s[i][j-1]-s[i-1][j-1],cout<<a[i][j]+s[i][j]<<' ';
+}
+void build_digits(long long D) {
+    for (int i=0;i<=9;i++) digits_D[i]=false;
+    while (D>0) {
+        digits_D[D%10]=true;
+        D/=10;
+    }
+}
+bool check_digits(long long x) {
+    while (x>0) {
+        int cif=x%10;
+        if (!digits_D[cif]) return false;
+        x/=10;
+    }
+    return true;
+}
+int main()
+{    fin>>c>>n>>D;
+    long long maxAi=0;
+    for (int i=1;i<=n;i++) {
+        fin>>a[i];
+        if (a[i]>maxAi) maxAi=a[i];
+    }
+    int cnt=0;
+    if (c==1) {
+        long long maxSum=maxAi+D;
+        build_sieve(maxSum);
+        for (int i=1;i<=n;i++) {
+            long long s=a[i]+D;
+            if (s<=maxSum && is_prime[s]) cnt++;
+        }
+    } else if (c==2) {
+        build_digits(D);
+        for (int i=1;i<=n;i++) {
+            if (check_digits(a[i])) cnt++;
+        }
+    }
+ fout<<cnt<<'\n';
+
+
     return 0;
 }
